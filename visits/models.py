871e1723f42a37970eb0062f673ca2a999a53498
Julia P <patterju@tcd.ie>
2013-03-09 15:58:59 +0000

    Use own Country model instead of django_countries

--- visits/models.py
+++ visits/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django_countries import CountryField
 from django.forms import ModelForm
 
 # Create your models here.
 
+#class Country(models.Model):
+	#country = CountryField(unique=True)
+
+
 class Visit(models.Model):
 	user = models.ForeignKey(User)
-	country = CountryField()
+	country = models.ForeignKey('Country')
+	#countries = models.ManyToManyField(CountryField)
 	visitDate = models.DateField('date of trip')
 	pub_date = models.DateTimeField('date published', auto_now = True)
 
+class Country(models.Model):
+	name = models.CharField(max_length = 256)
+	code = models.CharField(max_length = 8)
